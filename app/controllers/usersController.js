dde6840fbcc0906dba3d97345fe60177eac5c226
Quentinc63 <layk63@gmail.com>
2024-05-14 10:41:10 +0200

    fix: deleting index in models folder

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -4,7 +4,13 @@ const userController = {
     async getUserById(req, res) {
         const user = await User.findByPk(1)
         console.log(JSON.stringify(user))
+    },
+    async getAllUserWithRole(req,res){
+        const users = await User.findAll({
+            include:"role"
+        })
+        console.log(JSON.stringify(users))
     }
 };
-userController.getUserById()
+userController.getAllUserWithRole()
 export default userController;
