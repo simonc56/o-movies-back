6288f48e2c9477eb2067afb0fa17d65fa41b61d6
Quentinc63 <layk63@gmail.com>
2024-05-16 19:23:53 +0200

    fix: fixing some problem about getMoviesById

--- app/controllers/moviesController.js
+++ app/controllers/moviesController.js
@@ -2,33 +2,45 @@ import schema from '../validation/movieSchemas.js';
 import { Media } from '../models/associations.js';
 import validateData from '../validation/validator.js';
 import { fetchMovieTMDB } from '../services/axios.js';
-import axios, { AxiosError } from 'axios';
-import { User } from '../models/associations.js';
-import { Rating } from '../models/associations.js';
-import { Review } from '../models/associations.js';
-
-
+import axios from 'axios';
+import { User } from '../models/User.js';
+import { Review } from '../models/Review.js';
 const moviesController = {
     async getMoviesById(req, res) {
         try  {
-            const id = req.params.id;            
+            const id = req.params.id;         
+            // Validate the id parameter   
             const  { parsedData , errors }= validateData(parseInt(id), schema.getId);
+            // If there are errors, return a 400 response with the errors
             if (errors) {
                 return res.status(400).json({status: 'fail', data: errors });
             }
+            // Fetch the movie from the TMDB API
             const movie = await fetchMovieTMDB(`https://api.themoviedb.org/3/movie/${parsedData}?language=fr-FR`);
+            // If the response is an error, return a 400 response with the error message
             if (axios.isAxiosError(movie)) {
                 return res.status(400).json({status: 'fail', data: movie.message });
-            }
-            // i have a table media with tmdb_id and a table review who use the id of the media
-            const review = await Review.findAll({ where: { media_id: movie.id } });
-     
-             
-         
-
-            console.log(JSON.stringify(re, null, 2))
-
+            };      
             const cast = await fetchMovieTMDB(`https://api.themoviedb.org/3/movie/${parsedData}/credits?language=fr-FR`);
+            const media = await Media.findAll({
+                where: { tmdb_id: parsedData },
+                include: [
+                  {
+                    attributes: ['firstname'],
+                    association: 'users_rating',
+                    through: { attributes: ['id','value'] } 
+                  },
+                  {
+                    attributes: ['id', 'firstname'],
+                    association: 'users_review',
+                    through: { attributes: ['id','content','created_at'] } 
+                  }
+                ]
+              });
+            console.log(JSON.stringify(media, null, 2));               
+            //console.log(JSON.stringify(media.users_review, null, 2));
+            //console.log(JSON.stringify(media.users_review.map(review => review.review), null, 2));
+            //console.log(JSON.stringify(media.users_rating.map(rating => rating.rating), null, 2));           
             const data = {
                 id: movie.id,
                 title_fr: movie.title,
@@ -41,7 +53,12 @@ const moviesController = {
                 tagline : movie.tagline,
                 overview: movie.overview,
                 poster_path: `https://image.tmdb.org/t/p/w300_and_h450_bestv2/${movie.poster_path}`,
-                cast: cast.cast.map(actor => { return { id : actor.cast_id,name: actor.name, character: actor.character, profile_path: `https://image.tmdb.org/t/p/w300_and_h450_bestv2${actor.profile_path}` } }),
+                cast: cast.cast.map(actor => {
+                     return { 
+                        id : actor.cast_id,
+                        name: actor.name, 
+                        character: actor.character, 
+                        profile_path: `https://image.tmdb.org/t/p/w300_and_h450_bestv2${actor.profile_path}` } }),
                 crew: cast.crew
                 .filter(crew => crew.job === 'Director')
                 .map(crew => {
@@ -52,13 +69,13 @@ const moviesController = {
                         profile_path: `https://image.tmdb.org/t/p/w300_and_h450_bestv2${crew.profile_path}`
                     };
                 }),
-                review: more.users_review.map(review => { return { id: review.id, content: review.content } }),
+                reviews: media.users_review
             };            
             return res.json({status: 'success', data: data });
         }
         catch (error) {
             return res.status(400).json(error.message);
-        }
+        };
     }
 };
 
--- data/seeding.sql
+++ data/seeding.sql
@@ -21,11 +21,18 @@ INSERT INTO "playlist_has_media" ("playlist_id", "media_id") VALUES
 (2, 2);
 
 INSERT INTO "review" ("content", "user_id", "media_id") VALUES
-('So bad', 2, 1),
+('So bad', 2, 2),
 ('Lets gooo', 1, 2),
+('Great movie!', 1, 2),
+('Awesome show!', 2, 2),
+('Exciting stuff!', 1, 2),
+('Fantastic movie, loved it!', 2, 2),
+('So bad', 2, 1),
+('Lets gooo', 1, 1),
 ('Great movie!', 1, 1),
-('Awesome show!', 2, 2);
-
+('Awesome show!', 2, 1),
+('Exciting stuff!', 1, 1),
+('Fantastic movie, loved it!', 2, 1);
 
 INSERT INTO "view" ("user_id", "media_id") VALUES
 (1, 1),
