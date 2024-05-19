f9f3e9c1d6a86e7b4c2780215042fbe1120f8b6b
simonc56 <simchat@gmail.com>
2024-05-19 12:05:35 +0200

    chore: add linter and formatter rules

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    endOfLine: 'lf',
+    trailingComma: 'es5',
+    semi: true,
+    useTabs: false,
+    singleQuote: false,
+    quoteProps: 'preserve',
+    tabWidth: 4,
+    printWidth: 120,
+};
