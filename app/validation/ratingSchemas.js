d9ffd1a0196223935f646d0ad46d520421773447
Quentinc63 <layk63@gmail.com>
2024-05-22 14:49:19 +0200

    fix: fix value of rating can be decimal now

--- app/validation/ratingSchemas.js
+++ app/validation/ratingSchemas.js
@@ -2,11 +2,11 @@ import { z } from "zod";
 
 const schema = {
   createRatingSchema : z.object({
-    value: z.number().int().min(1).max(5), // rating must be an integer between 1 and 5 for the rating stars
+    value: z.number().min(0.5).max(5),
     tmdb_id : z.number().int().min(1),
   }),
   updateRatingSchema : z.object({ 
-    value: z.number().int().min(1).max(5),
+    value: z.number().min(0.5).max(5),
     ratingId: z.number().int().min(1),
     userId: z.number().int().min(1),  
   }),
