993d5ca548e02016817996c0088a812bc893b7b2
Quentinc63 <layk63@gmail.com>
2024-05-26 19:28:29 +0200

    fix: retiring node_env

--- app/middlewares/validationMiddleware.js
+++ app/middlewares/validationMiddleware.js
@@ -3,10 +3,8 @@ export function validationMiddleware(schema, requestProperty){
     try {
       schema.parse(req[requestProperty]);
       next();
-    } catch (error){
-      if (process.env.NODE_ENV === "production"){
-        console.error(error);
-      }
+    } catch (error){      
+      console.error(error);      
       return res.status(400).json({status:"fail", error: error.errors});         
     }
   };
