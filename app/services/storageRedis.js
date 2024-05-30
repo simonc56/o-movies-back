75528a45ee54236c373f882f34ff63b56d855d4d
Quentinc63 <layk63@gmail.com>
2024-05-30 17:19:47 +0200

    add: add a redis db for the cache

---
+++ app/services/storageRedis.js
@@ -0,0 +1,49 @@
+import  { createClient } from "redis";  // v4
+import { buildStorage, canStale } from "axios-cache-interceptor";
+
+const client = createClient({
+  password: "6LhB20N5xxNbmY3UmMFNtIxqBi9ytVww",
+  socket: {
+    host: "redis-16260.c135.eu-central-1-1.ec2.redns.redis-cloud.com",
+    port: 16260
+  }
+});
+
+const redisStorage = buildStorage({
+  find(key) {
+    return client
+      .get(`axios-cache-${key}`)
+      .then((result) => result && JSON.parse(result));
+  },
+  
+  set(key, value, req) {
+    return client.set(`axios-cache-${key}`, JSON.stringify(value), {
+      PXAT:
+
+        value.state === "loading"
+          ? Date.now() +
+                (req?.cache && typeof req.cache.ttl === "number"
+                  ? req.cache.ttl
+                  : 
+                  60000)
+          : 
+          (value.state === "stale" && value.ttl) ||
+                (value.state === "cached" && !canStale(value))
+            ?
+            value.createdAt + (value.ttl || 0)
+            : 
+            undefined
+    });
+  },
+  
+  remove(key) {
+    return client.del(`axios-cache-${key}`);
+  }
+});
+
+await client.connect();
+
+  
+
+export default redisStorage; 
+
