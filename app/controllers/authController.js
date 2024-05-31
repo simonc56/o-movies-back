9574433d33651e02dc604e1ab57baef99db89aee
Quentinc63 <153293172+Quentinc63@users.noreply.github.com>
2024-05-31 13:55:42 +0200

    Revert "add birthdate in returned userData at login" (#69)

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -1,58 +1,57 @@
-import bcrypt from "bcrypt"; // import the bcrypt package
+import { User } from "../models/User.js";   // import the User model from the models folder
+import bcrypt from "bcrypt"; // import the bcrypt package 
 import jwt from "jsonwebtoken"; // import the jsonwebtoken package
 import ApiError from "../errors/ApiError.js"; // import the ApiError class from the utils folder
-import { User } from "../models/User.js"; // import the User model from the models folder
 
 const authController = {
-  async registerUser(req, res, next) {
+  async registerUser(req, res,next) {
     // get data from request body
     const data = req.body;
-    // check if the email already exists
-    const existingUser = await User.findOne({ where: { email: data.email } });
+    // check if the email already exists 
+    const existingUser = await User.findOne({where: {email: data.email},});        
     if (existingUser) {
-      return next(new ApiError(400, "Email already exists"));
+      return next (new ApiError(400, "Email already exists"));
     }
     // hash the password
-    const hashedPassword = await bcrypt.hash(data.password, 10);
+    const hashedPassword = await bcrypt.hash(data.password, 10);           
     // create the user
     await User.create({
       role_id: 2,
       firstname: data.firstname,
       lastname: data.lastname,
-      email: data.email,
-      password: hashedPassword,
-      birthdate: data.birthdate,
-    });
+      email: data.email, 
+      password: hashedPassword, 
+      birthdate: data.birthdate
+    });            
     // return the user
     return res.json({ status: "success", data: true });
   },
 
-  async loginUser(req, res, next) {
+  async loginUser(req, res,next) {
     // get data from request body
-    const data = req.body;
+    const data = req.body;      
     // check if the email exists
-    const user = await User.findOne({ where: { email: data.email } });
+    const user = await User.findOne({ where: { email: data.email } });           
     if (!user) {
       return next(new ApiError(401, "Account not found"));
-    }
+    }           
     // check if the password is correct
     const validPassword = await bcrypt.compare(data.password, user.password);
     if (!validPassword) {
       return next(new ApiError(401, "Account not found"));
-    }
+    }           
     // create a token
-    const token = jwt.sign({ id: user.id }, process.env.TOKEN_SECRET);
+    const token = jwt.sign({ id: user.id }, process.env.TOKEN_SECRET);           
     // create the data user
     const dataUser = {
       firstname: user.firstname,
       lastname: user.lastname,
       email: user.email,
-      birthdate: user.birthdate,
-      token: token,
+      token: token
     };
-    // return the user
+      // return the user
     return res.json({ status: "success", data: dataUser });
-  },
+  }
 };
 
 export default authController;
