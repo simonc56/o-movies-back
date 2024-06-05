fd0a9ede95c1f146e1a0dc70a40d67fef024a6c4
hugoisidore <hugo.isidore@oclock.school>
2024-06-05 14:55:35 +0200

    feat: generic.schema.test.js done with mochai chai

--- app/validation/userSchemas.js
+++ app/validation/userSchemas.js
@@ -2,7 +2,6 @@ import { z } from "zod";
 
 // Define the regex for the password
 const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&,.<>^|+_-])(?=.*[a-zA-Z]).{8,}$/;
-const birthdateRegex = /^\d{4}-\d{2}-\d{2}$/;
 
 const schema = {
   registerSchema : z.object({       
@@ -10,7 +9,7 @@ const schema = {
     password: z.string().regex(passwordRegex, "password need to be : Minimum 8 characters, at least one uppercase letter, one lowercase letter, one number and one special character."), // Valide le mot de passe
     firstname: z.string().min(2).max(30), 
     lastname: z.string().min(2).max(30),
-    birthdate: z.string().regex(birthdateRegex, "birthdate must be in YYYY-MM-DD format"),        
+    birthdate: z.string().min(10).max(10)       
   }).required(),
   signInSchema : z.object({
     email: z.string().email(),
