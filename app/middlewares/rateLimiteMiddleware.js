ba1897b1a5f49f340349e3e0b302413a6efe030f
hugoisidore <hugo.isidore@oclock.school>
2024-06-05 16:17:43 +0200

    feat: ratelimit in windowMs to 40

--- app/middlewares/rateLimiteMiddleware.js
+++ app/middlewares/rateLimiteMiddleware.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 // Rate limiter middleware to limit the number of requests a user can make to the server
 const rateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, 
+  windowMs: 40 * 60 * 1000, 
   max: 5000, 
 });
 
