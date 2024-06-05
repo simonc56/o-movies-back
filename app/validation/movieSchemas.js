55c62f2cb8679b5ba842243cfa2e4c1d90e5eb54
Quentinc63 <layk63@gmail.com>
2024-06-05 17:06:48 +0200

    fix: fixing schema getMovieWithQueries

--- app/validation/movieSchemas.js
+++ app/validation/movieSchemas.js
@@ -10,7 +10,7 @@ const schema = {
       "vote_average.asc", "vote_average.desc", "vote_count.asc", "vote_count.desc"].includes(value),
     { message: "sort_by must be one of popularity.asc, popularity.desc, release_date.asc, release_date.desc, revenue.asc, revenue.desc, primary_release_date.asc, primary_release_date.desc, title.asc, title.desc, vote_average.asc, vote_average.desc, vote_count.asc, vote_count.desc" })
       .optional(),
-    with_genres: z.string().refine(value => value.split(",").every(id => parseInt(id) > 0), { message: "with_genres must be a list of positive integers separated by commas" }).optional(),     
+    with_genres: z.string().optional(),     
   }).strict(),
   getMovieSearch : z.object({
     query: z.string().min(1).max(50),
