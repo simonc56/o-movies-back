079caca634fcd890af430a30b32768175ca09f34
hugoisidore <hugo.isidore@oclock.school>
2024-06-05 16:04:21 +0200

    feat: rateLimit max to 5000

--- app/middlewares/rateLimiteMiddleware.js
+++ app/middlewares/rateLimiteMiddleware.js
@@ -3,7 +3,7 @@ import rateLimit from "express-rate-limit";
 // Rate limiter middleware to limit the number of requests a user can make to the server
 const rateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, 
-  max: 100, 
+  max: 5000, 
 });
 
 export default rateLimiter;
