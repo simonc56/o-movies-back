e02ac7ab3e4d49fcca91367bd5f026243e405b5c
simonc56 <simchat@gmail.com>
2024-06-11 22:04:31 +0200

    add schema for delete view

--- app/validation/viewSchemas.js
+++ app/validation/viewSchemas.js
@@ -1,9 +1,14 @@
 import { z } from "zod";
 
 const schema = {
-  viewedSchema : z.object({
-    tmdb_id: z.number().int().min(1),
-  }).required(),
+  viewedSchema: z
+    .object({
+      tmdb_id: z.number().int().min(1),
+    })
+    .required(),
+  unviewedSchema: z.object({
+    tmdb_id: z.string().refine((value) => parseInt(value) > 0, { message: "tmdb_id must be a positive integer" }),
+  }),
 };
 
 export default schema;
