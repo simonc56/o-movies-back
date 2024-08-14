f79c1ed8d67aa662ad724d2ae17202fc1ba968c1
simonc56 <simchat@gmail.com>
2024-08-14 14:01:37 +0200

    add optionalToken to continue if no token provided
    to not trigger an error (used for MoviePage)

--- app/middlewares/authMiddleware.js
+++ app/middlewares/authMiddleware.js
@@ -1,21 +1,58 @@
 import jwt from "jsonwebtoken";
 import ApiError from "../errors/ApiError.js";
+import { hashToken } from "../utils/auth.js";
 
-function verifyToken (req, res, next) {
-  const token = req.headers["authorization"]?.slice(7); // get the token from the header
-  if (!token) {
-    return next (new ApiError(403,"No token provided!" )); // if there isn't any token
-  }
+async function verifyToken(req, _, next) {
   try {
+    const token = req.headers["authorization"]?.slice(7);
+    const fingerprint = req.cookies?.fingerprint;
+    if (!token || !fingerprint) {
+      return next(new ApiError(401, "No token or fingerprint provided!"));
+    }
     const decoded = jwt.verify(token, process.env.TOKEN_SECRET); // verify token
+    const hashedFingerprint = hashToken(fingerprint);
+    if (decoded.fingerprint !== hashedFingerprint) {
+      return next(new ApiError(401, "Invalid fingerprint"));
+    }
     req.userId = decoded.id; // add the user id to the request
     next();
-  } catch (error){
-    if (error) {
-      console.error(error);
-      return next(new ApiError(401,"Unauthorized!" ));
+  } catch (error) {
+    console.error(error);
+    if (error instanceof jwt.JsonWebTokenError) {
+      return next(new ApiError(401, "Invalid token"));
+    } else if (error instanceof jwt.TokenExpiredError) {
+      return next(new ApiError(401, "Token expired"));
+    } else {
+      return next(new ApiError(500, "Internal server error"));
     }
   }
 }
 
-export default verifyToken;
+async function optionalToken(req, _, next) {
+  try {
+    const token = req.headers["authorization"]?.slice(7);
+    const fingerprint = req.cookies?.fingerprint;
+    if (!token || !fingerprint) {
+      console.log("No token or fingerprint provided!");
+      return next(); // If there's no token or fingerprint, just move on.
+    }
+    const decoded = jwt.decode(token, process.env.TOKEN_SECRET); // verify token
+    const hashedFingerprint = hashToken(fingerprint);
+    if (decoded.fingerprint !== hashedFingerprint) {
+      console.log("Invalid fingerprint");
+      return next(); // Invalid fingerprint, move on without error
+    }
+    req.userId = decoded.id; // add the user id to the request
+  } catch (error) {
+    // console.error(error);
+    // Handle specific JWT errors but do not throw them
+    if (error instanceof jwt.JsonWebTokenError || error instanceof jwt.TokenExpiredError) {
+      console.log("Invalid or expired token");
+      return next(); // Invalid token, move on without setting userId
+    }
+  }
+
+  next();
+}
+
+export { optionalToken, verifyToken };
