66b9a16b2fa67f40c39f62f5262ee4adddc5ab87
simonc56 <simchat@gmail.com>
2025-10-26 14:07:13 +0100

    fix: body sanitizer middleware check for object type before sanitization (express 5)

--- app/middlewares/sanitizeMiddleware.js
+++ app/middlewares/sanitizeMiddleware.js
@@ -2,11 +2,13 @@ import sanitizeHtml from "sanitize-html";
 
 // Sanitize the body of the request to prevent XSS attacks
 const bodySanitizer = (req, res, next) => {
-  Object.keys(req.body).forEach(key => { 
-    if (typeof req.body[key] === "string") {
-      req.body[key] = sanitizeHtml(req.body[key]);
-    }
-  });
+  if (req.body && typeof req.body === "object") {
+    Object.keys(req.body).forEach((key) => {
+      if (typeof req.body[key] === "string") {
+        req.body[key] = sanitizeHtml(req.body[key]);
+      }
+    });
+  }
   next();
 };
 
